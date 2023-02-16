5cd0e3da6c765ac2b43d24d580856290584583d6
bobbrom <bobbrom@gmail.com>
2023-02-16 18:08:15 +0000

    Get rid of console logs in countdown.js

--- static/countdown.js
+++ static/countdown.js
@@ -3,8 +3,6 @@ $(document).ready(function() {
     var messageCountdownElements = document.getElementsByClassName("message-countdown")
 
     for (var i = 1; i < messageDateTimeElements.length; i++) {
-        console.log(messageDateTimeElements[i].innerHTML)
-        console.log(moment(messageDateTimeElements[i].innerHTML, "DD/MM/YYYY {hh:mm:ss}"))
         var count_down = moment(messageDateTimeElements[i].innerHTML, "DD/MM/YYYY {hh:mm:ss}").fromNow()
         messageCountdownElements[i].innerHTML = count_down
     }
